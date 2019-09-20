07ddc707c33eb9a9b667edd401f4e98876dfd83f
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-20 08:27:02 -0700

    basic form created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [creds, setCreds] = useState({
+    username: "",
+    password: ""
+  })
+  const handleChange = e =>{
+    setCreds({
+      ...creds,
+      [e.target.name] : e.target.value
+    })
+
+  }
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h1>Login to see bubbles!</h1>
+      <form>
+        <input placeholder="Enter Username..." name="username" value ={creds.username} onChange ={handleChange}/>
+        <input placeholder="Enter Password..." name="password" value ={creds.password} onChange={handleChange}/>
+        <button>Submit</button>
+      </form>
     </>
   );
 };
